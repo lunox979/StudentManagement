b766fbae129e700957a6d0f84160545e86a16fcd
lunox979 <yuyuto55555@gmail.com>
2025-11-18 18:11:04 +0900

    feat: 学生情報の取得と登録の追加(web)

---
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -0,0 +1,29 @@
+package raisetech.student.management;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@Mapper
+public interface StudentRepository {
+
+
+
+  @Select("SELECT * FROgM student")
+  List<Student> getStudentList();
+
+
+  @Insert("INSERT student values(#{name}, #{age})")
+  void registerStudent(String name, Integer age);
+
+  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
+  void updateStudent(String name, Integer age);
+
+  @Delete("DELETE FROM student WHERE name = #{name}")
+  void deleteStudent(String name);
+
+}
